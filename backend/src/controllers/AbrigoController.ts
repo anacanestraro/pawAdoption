d927a2234da5e1d098a1f92408542a2da291dd28
anacanestraro <ana.canestraro07@gmail.com>
2025-10-07 15:05:00 -0300

    feat: cadastrar abrigo

--- backend/src/controllers/AbrigoController.ts
+++ backend/src/controllers/AbrigoController.ts
@@ -18,3 +18,60 @@ export const listarAbrigos = async (req: Request, res: Response) => {
         return res.status(500).json({error: "Erro ao buscar abrigos"});
     }
 }
+
+export const cadastrarAbrigo = async(req: Request, res: Response) =>  {
+    const dados: NovoAbrigoDTO = req.body;
+
+    try {
+        const  senhaHash = await bcrypt.hash(dados.senha, 10);
+
+        const result = await prisma.$transaction(async (tx) =>{
+            const usuario = await tx.usuario.create({
+                data: {
+                    email: dados.email,
+                    senha_hash: senhaHash,
+                    nome: dados.nome,
+                    telefone: dados.telefone,
+                    tipo_usuario: "ABRIGO",
+                    endereco: dados.endereco
+                    ? {
+                        create: {
+                            cep: dados.endereco.cep,
+                            logradouro: dados.endereco.logradouro,
+                            numero: dados.endereco.numero,
+                            complemento: dados.endereco.complemento,
+                            bairro: dados.endereco.bairro,
+                            cidade: dados.endereco.cidade,
+                            estado: dados.endereco.estado,
+                        },
+                    }
+                    : undefined,
+                },
+            });
+
+            const abrigo = await tx.abrigo.create({
+                data: {
+                    usuario_id: usuario.id,
+                    cnpj: dados.cnpj,
+                    razao_social: dados.razao_social,
+                    capacidade: dados.capacidade,
+                    sobre: dados.sobre,
+                    site_url: dados.site_url,
+                    redes_sociais: dados.redes_sociais,
+                },
+                include: {
+                    usuario: {
+                        include: {
+                            endereco: true,
+                        },
+                    },
+                },
+            });
+            return abrigo;
+        });
+
+        return res.status(201).json(result);
+    }catch(error){
+        return res.status(500).json({error: "Erro ao cadastrar abrigo"});
+    }
+}
--- backend/src/routes/AbrigoRoute.ts
+++ backend/src/routes/AbrigoRoute.ts
@@ -4,6 +4,7 @@ import * as AbrigoController from "../controllers/AbrigoController";
 const router = Router();
 
 router.get("/", AbrigoController.listarAbrigos);
+router.post("/cadastrar", AbrigoController.cadastrarAbrigo);
 
 export default router;
 
