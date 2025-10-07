85dd8e3523f542b402e40a7d7aab249db5df4806
anacanestraro <ana.canestraro07@gmail.com>
2025-10-07 13:46:30 -0300

    fix: cpf adotante obrigatório

--- backend/src/dtos/AdotanteDTO.ts
+++ backend/src/dtos/AdotanteDTO.ts
@@ -2,7 +2,7 @@ import { EnderecoDTO } from "./EnderecoDTO";
 import { NovoUsuarioDTO } from "./UsuarioDTO";
 
 export interface NovoAdotanteDTO extends Omit<NovoUsuarioDTO, "tipo_usuario"> {
-  cpf?: string;
+  cpf: string;
   data_nascimento?: Date;
   lar_temporario?: boolean;
   capacidade_lar_temporario?: number;
