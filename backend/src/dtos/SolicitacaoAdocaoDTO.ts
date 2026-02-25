2041030d410af671d698eb7bb1867cfe600ca530
anacanestraro <ana.canestraro07@gmail.com>
2026-02-25 15:30:11 -0300

    Related #16: Ajuste no DTO de solicitação e adicionando um dto para atualizaçaõ de solicitação

--- backend/src/dtos/SolicitacaoAdocaoDTO.ts
+++ backend/src/dtos/SolicitacaoAdocaoDTO.ts
@@ -1,5 +1,6 @@
 export interface NovaSolicitacaoDTO {
     animal_id: number;
-    adotante_id: number;
-    status?: 'PENDENTE' | 'APROVADA' | 'REJEITADA' | 'CANCELADA';
+}
+export interface AtualizarSolicitacaoDTO {
+    status: 'APROVADA' | 'REJEITADA' | 'CANCELADA';
 }
