ab78c68fa96675f56a04fbb20b9da296d5c1a925
anacanestraro <ana.canestraro07@gmail.com>
2026-02-28 22:45:15 -0300

    Related #06: Implementação da listagem de denuncias

---
+++ backend/src/controllers/DenunciaController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import prisma from "../lib/client";
+import { AuthRequest } from "../middlewares/auth";
+import { NovaDenunciaDTO, AtualizarDenunciaDTO } from "../dtos/DenunciaDTO";
+
+export const listarDenuncias = async (req: Request, res:Response) => {
+    try {
+        const denuncias = await prisma.denuncia.findMany({
+            include: {
+                animal: true,
+                usuario: true,
+            }
+        });
+        return res.status(200).json(denuncias);
+    } catch(error) {
+        return res.status(500).json({ error: "Erro ao buscar denuncias"});
+    }
+}
