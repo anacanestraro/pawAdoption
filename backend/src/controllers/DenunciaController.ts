2b7c43ce32f8021d7e3fe56122827bd3660ec49a
anacanestraro <ana.canestraro07@gmail.com>
2026-03-05 22:30:25 -0300

    Related #06: Implementação da função de denunciar um anuncio de animal

--- backend/src/controllers/DenunciaController.ts
+++ backend/src/controllers/DenunciaController.ts
@@ -16,3 +16,36 @@ export const listarDenuncias = async (req: Request, res:Response) => {
         return res.status(500).json({ error: "Erro ao buscar denuncias"});
     }
 }
+
+export const denunciar = async (req:AuthRequest<NovaDenunciaDTO>, res: Response) => {
+    const { id } = req.params;
+    const { motivo } = req.body;
+
+    try {
+        if (!req.usuario) {
+            return res.status(401).json({ error: "Usuário não autenticado"});
+        }
+
+        const animal = await prisma.animal.findUnique({
+            where: {id: Number(id)}
+        });
+
+        if (!animal) {
+            return res.status(404).json({error: "Animal não encontrado"})
+        }
+
+        const novaDenuncia = await prisma.denuncia.create({
+            data: {
+                animal_id: Number(id),
+                usuario_denunciante_id: req.usuario.id,
+                motivo,
+                status: 'PENDENTE'
+            }
+        });
+
+        return res.status(201).json(novaDenuncia);
+
+    } catch(error) {
+        return res.status(500).json({ error: "Erro ao denunciar"});
+    }
+}
