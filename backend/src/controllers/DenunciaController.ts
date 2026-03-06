126e8a07f88259c35e86c4d820c0652b8863fb97
anacanestraro <ana.canestraro07@gmail.com>
2026-03-05 22:56:59 -0300

    Related #06: Implementação da funcionalidade de resolver uma denuncia

--- backend/src/controllers/DenunciaController.ts
+++ backend/src/controllers/DenunciaController.ts
@@ -49,3 +49,42 @@ export const denunciar = async (req:AuthRequest<NovaDenunciaDTO>, res: Response)
         return res.status(500).json({ error: "Erro ao denunciar"});
     }
 }
+
+export const resolverDenuncia = async (req:AuthRequest<AtualizarDenunciaDTO>, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        if (!req.usuario) {
+            return res.status(401).json({error: "Usuário não autenticado"});
+        }
+        
+        if (req.usuario.tipo_usuario !== 'ADMINISTRADOR' ) {
+            return res.status(403).json({error: "Apenas administradores podem resolver uma denuncia"})
+        }
+
+        const denuncia = await prisma.denuncia.findUnique({
+            where: { id: Number(id) }
+        });
+
+        if (!denuncia) {
+            return res.status(404).json({error: "Denuncia não encontrada"})
+        }
+        
+        if (denuncia.status == 'RESOLVIDA') {
+            return res.status(400).json({error: "Denuncia já foi resolvida"})
+        }
+
+        const denunciaAtualizada = await prisma.denuncia.update({
+            where: { id: Number(id) },
+            data: {
+                status: 'RESOLVIDA',
+                admin_responsavel_id: req.usuario.id,
+                data_resolucao: new Date()
+            }
+        });
+
+        return res.status(200).json(denunciaAtualizada)
+    } catch(error) {
+        return res.status(500).json({error: "Erro ao resolver denuncia"})
+    }
+}
