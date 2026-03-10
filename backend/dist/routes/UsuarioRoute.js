7216f8bcf546945b87df905e4d025c7404d64d2c
anacanestraro <ana.canestraro07@gmail.com>
2026-03-10 15:59:15 -0300

    Closes #23: Removendo dist e adicionando ele no gitignore

--- backend/dist/routes/UsuarioRoute.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import * as UsuarioController from "../controllers/UsuarioController.js";
-const router = Router();
-router.get("/listarUsuarios", UsuarioController.listarUsuarios);
-export default router;
-//# sourceMappingURL=UsuarioRoute.js.map
