eff8c2187149f3dab3abb1e58e6404c24d09d173
anacanestraro <ana.canestraro07@gmail.com>
2026-03-10 16:22:51 -0300

    Related #18: Ajuste na função de listarVoluntarios para listar voluntarios por abrigo

--- backend/src/controllers/VoluntarioController.ts
+++ backend/src/controllers/VoluntarioController.ts
@@ -3,16 +3,21 @@ import prisma from "../lib/client";
 import { AuthRequest } from "../middlewares/auth";
 import { NovoVoluntarioDto, AtualizarVoluntarioDTO } from "../dtos/VoluntarioDTO";
 
-export const listarVoluntarios = async (req: Request, res: Response) => {
+export const listarVoluntariosDoAbrigo = async (req: Request, res: Response) => {
     try {
+
+        const { abrigo_id } = req.params;
         const voluntarios = await prisma.voluntario.findMany({
+            where: {
+                abrigo_id: Number(abrigo_id),
+                status: 'ATIVO'
+            },
             include: {
                 adotante: true,
-                abrigo: true,
             }
         });
         return res.status(200).json(voluntarios);
     } catch (error) {
-        return res.status(500).json({ error: "Erro ao buscar voluntários" });
+        return res.status(500).json({ error: "Erro ao buscar voluntários do abrigo" });
     }
 }
