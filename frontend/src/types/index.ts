c321eb85b1044a82f0a0d11f5209e9fb93422c8a
anacanestraro <ana.canestraro07@gmail.com>
2026-03-12 18:06:29 -0300

    Related #27: Implementação do type de Animal

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -4,3 +4,18 @@ export interface Usuario {
     email: string;
     tipo_usuario: 'ADOTANTE' | 'ABRIGO' | 'ADMINISTRADOR';
 }
+
+export interface Animal {
+    id: number;
+    nome: string;
+    especie: string;
+    raca?: string;
+    idade?: number;
+    porte?: 'PEQUENO' | 'MEDIO' | 'GRANDE';
+    sexo?: 'MACHO' | 'FEMEA';
+    descricao?: string;
+    status: 'PENDENTE' | 'DISPONIVEL' | 'ADOTADO' | 'PROCESSO_ADOCAO'
+    abrigo_id?: number;
+    adotante_id?: number
+    lar_temporario?: number;
+}
