569a6d79cf47e6722c86c7f807b3d8624879d8c1
anacanestraro <ana.canestraro07@gmail.com>
2026-03-17 21:57:17 -0300

    Related #29: Ajustando as rotas públicas do sistema

--- frontend/src/routes/PublicRoutes.tsx
+++ frontend/src/routes/PublicRoutes.tsx
@@ -1,10 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { Login } from '../pages/Login'
+import { useAuth } from '../context/AuthContext'
 
 export const PublicRoutes = () => {
+
+    const { token, usuario } = useAuth()
+
+    if (token && usuario) {
+        return <Navigate to={usuario.tipo_usuario === 'ADMINISTRADOR' ? '/admin' : '/home'} replace />
+    }
+
     return (
         <Routes>
-            <Route path="/" element={<h1>Home</h1>}/>
-            <Route path="/login" element={<h1>Login</h1>}/>
+            <Route path="/login" element={<Login />} />
+            <Route path="/cadastro" element={<h1>Cadastro</h1>}/>
+            <Route path="*" element={<Navigate to="/login" replace/>}/>
         </Routes>
     )
 }
