800a5be9060032b503a4a2c2ed97a5e6508d1bc4
anacanestraro <ana.canestraro07@gmail.com>
2026-04-05 19:44:11 -0300

    Related #26: Importando o bootstrap no main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles/bootstrap-custom.scss'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
